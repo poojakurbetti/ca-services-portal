a3383a6d977d1c93e8e5dedd107b4dd5ecbc2624
pooja K <poojak@poojas-MacBook-Air.local>
2025-08-14 13:25:19 +0530

    front end code

--- client/api/api.js
+++ client/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api';
+const API_URL = 'http://localhost:5002/api';
 
 // Services
 export const fetchServices = () => axios.get(`${API_URL}/services`);
